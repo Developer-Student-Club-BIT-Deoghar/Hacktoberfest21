96d32afe4f92f9775cbdd50d49980807dfab2c26
Akash Dip <77287683+kod-it@users.noreply.github.com>
2021-09-30 20:50:27 +0530

    initial-commits

---
+++ task1/js/index.js
@@ -0,0 +1,9 @@
+(function(){
+    const divs = document.getElementsByClassName('card__image');
+    for (let i = 0; i < divs.length; i++) {
+        const div = divs[i];
+        const img = div.getElementsByTagName('img')[0]
+                        .getAttribute('src');
+        div.style.backgroundImage = 'url(' + img + ')';
+    }
+}());
